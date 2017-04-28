d91fdc7c1aee2ec6543fb45d0d163313b83f81f6
studiousryan <studiousryan@gmail.com>
2017-04-28 00:22:29 -0700

    changed signup form input-error color from blue to red

--- index.php
+++ index.php
@@ -11,15 +11,26 @@ session_start();
 	<title>Artist Inventory</title>
 </head>
 <body>
-	<header>
-		<nav>
-			<ul>
-				<li><a id='btnToLoginPage' href="login.php"><span class="glyphicon glyphicon-log-in"></span> Log in</a></li>
-				<li><a id='logoutBtn' href="logoutHandler.php"><span class="glyphicon glyphicon-log-out"></span> Log out</a></li>
-				<!-- <p id='greeting'>Welcome back, <span>$_SESSION['username']</span>.</p> -->
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+		  		<a class="navbar-brand" href="index.php">Artist Inventory</a>
+			</div>
+			<ul class="nav navbar-nav navbar-right">
+			 	<li class="active"><a href="index.php">Home</a></li>
+			 	<li><a href="myInventory.php">My Inventory</a></li>
+			 	<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Sample Page<span class="caret"></span></a>
+				    <ul class="dropdown-menu">
+				     	<li><a href="#">Page 1-1</a></li>
+				     	<li><a href="#">Page 1-2</a></li>
+				    	<li><a href="#">Page 1-3</a></li>
+				    </ul>
+				</li>
+				<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+		 		<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
 			</ul>
-		</nav>
-	</header>
+		</div>
+	</nav>
 
 	<div id='mainBody'>
 		<div class='credential-window' id='signup-window'>
